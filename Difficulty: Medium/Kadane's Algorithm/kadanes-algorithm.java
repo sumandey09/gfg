a28b6f439e1c3b9d5b00aebd3d5a9501786d30b1
Suman Dey <147734727+sumandey09@users.noreply.github.com>
2024-09-06 18:43:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.maxSubarraySum(arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+
+    int maxSubarraySum(int[] arr) {
+        int maxSum = arr[0];  
+        int currentSum = arr[0];  
+        
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+}
+
