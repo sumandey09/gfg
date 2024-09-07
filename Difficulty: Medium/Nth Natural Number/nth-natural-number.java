e61fac3c79a0592c5009feb5ee4e6c7087452615
Suman Dey <147734727+sumandey09@users.noreply.github.com>
2024-09-07 16:49:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nth Natural Number/nth-natural-number.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        int t = ob.nextInt();
+        while (t-- > 0) {
+            long n = ob.nextLong();
+            Solution ab = new Solution();
+            long k = ab.findNth(n);
+            System.out.println(k);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    long findNth(long n) {
+        // code her
+        long p=1;
+        long ans=0;
+        while(n>0){
+            ans+=p*(n%9);
+            n/=9;
+            p*=10;
+        }
+        return ans;
+    }
+}
