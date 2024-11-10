5a701359be76b9ec972c9f42dbeca6de00eebbc2
Suman Dey <147734727+sumandey09@users.noreply.github.com>
2024-11-10 17:02:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Two Sorted Arrays with Distinct Elements/union-of-two-sorted-arrays-with-distinct-elements.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            // First array input
+            String[] str1 = br.readLine().trim().split(
+                " "); // Read the first line and split by spaces
+            int n = str1.length;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str1[i]); // Convert each element to an integer
+            }
+
+            // Second array input
+            String[] str2 = br.readLine().trim().split(
+                " "); // Read the second line and split by spaces
+            int m = str2.length;
+            int[] b = new int[m];
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(str2[i]); // Convert each element to an integer
+            }
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = obj.findUnion(a, b);
+            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+// a,b : the arrays
+class Solution {
+    // Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        // add your code here
+        int i=0,j=0,n=a.length,m=b.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(i<n || j<m){
+            int num1 = i<n?a[i]:Integer.MAX_VALUE;
+            int num2 = j<m?b[j]:Integer.MAX_VALUE;
+            if(num1<num2){
+                ans.add(num1);
+                i++;
+            }
+            else if(num2<num1){
+                ans.add(num2);
+                j++;
+            }
+            else{
+                ans.add(num1);
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+}
